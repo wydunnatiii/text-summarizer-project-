7c7d046e981ae5842945e7b87d57d5ba67e3e7ad
Unnati pal <unnatipal@MacBook-Air.local>
2024-07-15 12:24:18 +0530

    requirements & installation done

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8")as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "text-summarizer-project-"
+AUTHOR_USER_NAME = "wydunnatiii"
+SRC_REPO = "textSummarizer"
+AUTHOR_EMAIL = "palunnati90@gmail.com"
+
+
+setuptools.setup(
+    name=SRC_REPO,
+    version=__version__,
+    author=AUTHOR_USER_NAME,
+    author_email=AUTHOR_EMAIL,
+    description="A small python package for NLP app",
+    Long_description=long_description,
+    Long_description_content="text/markdoen",
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
+    project_urls={
+        "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
+
+    },
+    package_dir={"": "src"},
+    package=setuptools.find_packages(where="src")
+
+
+
+)
